44e00b6af3e2f9c9b3fe0f5965f8e8320e62f907
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-11-28 13:56:57 +0200

    create contact schema

---
+++ src/models/crmModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema({
+    firstName: {
+        type: String,
+        required: "Enter the first name",
+    },
+    lastName: {
+        type: String,
+        required: "Enter the last name",
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    company: {
+        type: String,
+    },
+    created_date: {
+        type: Date,
+        default: Date.now(),
+    },
+})
