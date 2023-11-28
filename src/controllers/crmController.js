9cd8c318b22b80a3abc57ea5c06d3a66e52bbac8
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-11-28 14:24:48 +0200

    create getContacts Controller

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -4,14 +4,22 @@ import { ContactSchema } from '../models/crmModel';
 
 const Contact = mongoose.model('Contact', ContactSchema);
 
-export const addNewContact = (req, res) => {
+export const addNewContact = async (req, res) => {
     let newContact = new Contact(req.body);
 
-    newContact.save((err, contact) => {
-        if (err){
-            res.send(err)
-        }
-        
-        res.json(contact)
-    })
+    try {
+        let addedContact = await newContact.save();
+        res.json(addedContact);
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+export const getContacts = async (req, res) => {
+    try {
+        let contacts = await Contact.find({});
+        res.send(contacts);
+    } catch (err) {
+        res.send(err);
+    }
 }
--- src/routes/crmRoute.js
+++ src/routes/crmRoute.js
@@ -1,16 +1,9 @@
+import { addNewContact, getContacts } from "../controllers/crmController";
+
 const routes = (app) => {
     app.route('/contact')
-        .get((req, res, next) => {
-            console.log(req.originalUrl);
-            console.log(req.method);
-            next();
-        }, (req, res, next)=>{
-            res.send("GET request succeed!");
-        })
-
-        .post((req, res) => {
-            res.send("POST request succeed!");
-        })
+        .get(getContacts)
+        .post(addNewContact)
 
     app.route('/contact/:contactId')
         .put((req, res) => {
